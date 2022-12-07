a5d41141b328282b579d2e4b886bab8bbc29edbb
pogist <murilo.paixao.2@gmail.com>
2022-12-07 14:03:14 -0300

    setup a few more lsps

--- lua/lsp/configs.lua
+++ lua/lsp/configs.lua
@@ -4,6 +4,7 @@ require('mason-lspconfig').setup()
 local lspconfig = require('lspconfig')
 local capabilities = require('cmp_nvim_lsp').default_capabilities()
 
+-- Lua
 lspconfig.sumneko_lua.setup {
   capabilities = capabilities,
   settings = {
@@ -27,16 +28,77 @@ lspconfig.sumneko_lua.setup {
   }
 }
 
+-- CSS
 lspconfig.cssls.setup {
   capabilities = capabilities,
 }
 
-lspconfig.cssmodules_ls.setup{}
-lspconfig.cmake.setup{}
-lspconfig.dockerls.setup{}
+-- CSS modules
+lspconfig.cssmodules_ls.setup {
+  capabilities = capabilities,
+  filetypes = {
+    'javascript.jsx',
+    'javascriptreact',
+    'typescript.tsx',
+    'typescriptreact',
+  },
+}
 
+-- CMake
+lspconfig.cmake.setup {
+  capabilities = capabilities,
+}
+
+-- Dockerfile
+lspconfig.dockerls.setup {
+  capabilities = capabilities,
+}
+
+-- C/C++/Obj-C
 lspconfig.ccls.setup {
+  capabilities = capabilities,
   init_options = {
     compilationDatabaseDirectory = 'build',
   },
+}
+
+-- Graphql
+lspconfig.graphql.setup {
+  capabilities = capabilities,
+}
+
+-- Html
+lspconfig.html.setup {
+  capabilities = capabilities,
+}
+
+-- JSON
+lspconfig.jsonls.setup {
+  capabilities = capabilities,
+}
+
+-- Ruby
+lspconfig.solargraph.setup {
+  capabilities = capabilities,
+}
+
+-- Swift
+lspconfig.sourcekit.setup {
+  capabilities = capabilities,
+  filetypes = {'swift'},
+}
+
+-- JavaScript/TypeScript
+lspconfig.tsserver.setup {
+  capabilities = capabilities,
+}
+
+-- YAML
+lspconfig.yamlls.setup {
+  capabilities = capabilities,
+}
+
+-- Vim
+lspconfig.vimls.setup {
+  capabilities = capabilities,
 }

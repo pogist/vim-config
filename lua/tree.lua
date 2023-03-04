55332a9310e9a66e575807f07498ae31b9b989b6
pogist <murilo.paixao.2@gmail.com>
2023-03-04 16:37:29 -0300

    add vsplit shortcut for nvim-tree

--- lua/tree.lua
+++ lua/tree.lua
@@ -1,3 +1,17 @@
+local lib = require 'nvim-tree.lib'
+
+local function vsplit_preview()
+  local action = 'vsplit'
+  local node = lib.get_node_at_cursor()
+  if node.link_to and not node.nodes then
+    require('nvim-tree.actions.node.open-file').fn(action, node.link_to)
+  elseif node.nodes ~= nil then
+    lib.expand_or_collapse(node)
+  else
+    require('nvim-tree.actions.node.open-file').fn(action, node.absolute_path)
+  end
+end
+
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
@@ -9,6 +23,12 @@ require('nvim-tree').setup {
     centralize_selection = true,
     hide_root_folder = true,
     relativenumber = true,
+    mappings = {
+      custom_only = false,
+      list = {
+        { key = 's', action = 'vsplit_preview', action_cb = vsplit_preview },
+      },
+    },
   },
   renderer = {
     add_trailing = true,

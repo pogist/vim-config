49a39697eca3248155df7cdcb78bf74c72baaa02
pogist <murilo.paixao.2@gmail.com>
2023-12-20 19:36:30 -0300

    enable formatting through null-ls

--- lua/lsp/formatters.lua
+++ lua/lsp/formatters.lua
@@ -2,8 +2,29 @@ local null_ls = require 'null-ls'
 local null_ls_utils = require 'null-ls.utils'
 local node_utils = require 'utils.node'
 
+local augroup = vim.api.nvim_create_augroup('UserLspFormatting', {})
+local function on_attach(client, bufnr)
+  if client.supports_method 'textDocument/formatting' then
+    vim.api.nvim_clear_autocmds { group = augroup, buffer = bufnr }
+    vim.api.nvim_create_autocmd('BufWritePre', {
+      group = augroup,
+      buffer = bufnr,
+      callback = function()
+        vim.lsp.buf.format {
+          async = false,
+          filter = function(c)
+            return c.name ~= 'tsserver'
+          end,
+        }
+      end,
+    })
+  end
+end
+
 null_ls.setup {
+  on_attach,
   sources = {
+    null_ls.builtins.formatting.shfmt,
     null_ls.builtins.formatting.stylua,
     null_ls.builtins.formatting.prettierd.with {
       condition = node_utils.condition {

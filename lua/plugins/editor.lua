2b789e01031c1e8d5a1d00aae0a216ba02ca5de1
pogist <murilo.paixao.2@gmail.com>
2023-12-21 22:52:31 -0300

    add editor plugins and nvim-tree

---
+++ lua/plugins/editor.lua
@@ -0,0 +1,54 @@
+return {
+  {
+    "nvim-tree/nvim-tree.lua",
+    version = false,
+    dependencies = { "nvim-tree/nvim-web-devicons" },
+    keys = {
+      {
+        "<C-n>",
+        function()
+          require("nvim-tree.api").tree.toggle()
+        end,
+      },
+    },
+    opts = {
+      diagnostics = { enable = true },
+      git = { ignore = false },
+      view = {
+        adaptive_size = true,
+        relativenumber = true,
+        centralize_selection = true,
+      },
+      renderer = {
+        add_trailing = true,
+      },
+      on_attach = function(bufnr)
+        local api = require("nvim-tree.api")
+        -- set plugin's default mappings
+        api.config.mappings.default_on_attach(bufnr)
+        -- override some of plugin's default mappings
+        local opts = { buffer = bufnr, noremap = true, nowait = true, silent = true }
+        vim.keymap.set("n", "s", api.node.open.vertical, opts)
+        vim.keymap.set("n", "Z", api.node.run.system, opts)
+      end,
+    },
+    -- Autocommand that detects if NvimTree is the only visible window
+    -- when entering it's a buffer. This is useful to detect if the user
+    -- is leaving the editor with the tree still open and auto close it.
+    init = function()
+      vim.api.nvim_create_autocmd("BufEnter", {
+        group = vim.api.nvim_create_augroup("NvimTreeAutoClose", { clear = true }),
+        callback = function()
+          local layout = vim.api.nvim_call_function("winlayout", {})
+          if layout[1] == "leaf" then
+            local buf = vim.api.nvim_win_get_buf(layout[2])
+            local buf_ft = vim.api.nvim_buf_get_option(buf, "filetype")
+            if buf_ft:match("NvimTree") ~= nil and layout[3] == nil then
+              vim.cmd("confirm quit")
+            end
+          end
+        end,
+      })
+    end,
+  },
+}

be4eca37e9fdbcfe3754a12cd3215fd01e5b6381
pogist <murilo.paixao.2@gmail.com>
2023-12-30 14:01:43 -0300

    move utility function to a separate file

--- lua/plugins/coding.lua
+++ lua/plugins/coding.lua
@@ -1,3 +1,4 @@
+local util = require('../util')
 return {
   {
     "echasnovski/mini.pairs",
@@ -90,19 +91,11 @@ return {
             end
           end,
           ["<Tab>"] = function(fallback)
-            local has_word_at_cursor = function()
-              local row, col = unpack(vim.api.nvim_win_get_cursor(0))
-              if col == 0 then
-                return false
-              end
-              local line = vim.api.nvim_buf_get_lines(0, row - 1, row, true)[1]
-              return not line:sub(col, col):match("%s")
-            end
             if cmp.visible() then
               cmp.select_next_item()
             elseif luasnip.expand_or_jumpable() then
               luasnip.expand_or_jump()
-            elseif has_word_at_cursor() then
+            elseif util.has_word_at_cursor() then
               cmp.complete()
             else
               fallback()
---
+++ lua/util.lua
@@ -0,0 +1,12 @@
+local M = {}
+
+M.has_word_at_cursor = function()
+  local row, col = unpack(vim.api.nvim_win_get_cursor(0))
+  if col == 0 then
+    return false
+  end
+  local current_line = vim.api.nvim_buf_get_lines(0, row - 1, row, true)[1]
+  return not current_line:sub(col, col):match("%s")
+end
+
+return M

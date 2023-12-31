8de69b3e4a71306b4ee4870a95bda2a9ccc07db4
pogist <murilo.paixao.2@gmail.com>
2023-12-31 20:05:36 -0300

    setup vim-illuminate

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -98,8 +98,46 @@ return {
         map("n", "<leader>hd", gs.diffthis, { desc = "Diff This" })
         map("n", "<leader>td", gs.toggle_deleted, { desc = "Toggle Deleted" })
         -- Text object
-        map({ 'o', 'x' }, 'ih', '<Cmd><C-U>Gitsigns select_hunk<CR>')
+        map({ "o", "x" }, "ih", "<Cmd><C-U>Gitsigns select_hunk<CR>")
       end,
     },
+  },
+  {
+    "RRethy/vim-illuminate",
+    version = false,
+    event = "VeryLazy",
+    opts = {
+      large_file_cutoff = 2000,
+      large_file_overrides = {
+        providers = { "lsp" },
+      },
+      min_count_to_highlight = 2,
+    },
+    keys = {
+      { "]]", desc = "Next Reference" },
+      { "[[", desc = "Prev Reference" },
+    },
+    config = function(_, opts)
+      local illuminate = require("illuminate")
+      illuminate.configure(opts)
+
+      local function create_maps(buffer)
+        vim.keymap.set("n", "]]", function()
+          illuminate.goto_next_reference(false)
+        end, { desc = "Next Reference", buffer = buffer })
+        vim.keymap.set("n", "[[", function()
+          illuminate.goto_prev_reference(false)
+        end, { desc = "Prev Reference", buffer = buffer })
+      end
+
+      create_maps()
+
+      -- also set it after loading ftplugins, since a lot overwrite [[ and ]]
+      vim.api.nvim_create_autocmd("FileType", {
+        callback = function()
+          create_maps(vim.api.nvim_get_current_buf())
+        end,
+      })
+    end,
   },
 }

fd764577345a88265fa76adc929e1091e9734b9e
pogist <murilo.paixao.2@gmail.com>
2024-02-05 13:47:15 -0300

    improve formatting opts and sort CloseWithQ filetypes

--- lua/plugins/formatting.lua
+++ lua/plugins/formatting.lua
@@ -14,34 +14,32 @@ return {
       },
     },
     opts = {
-      configs = {
-        {
-          formatter = { "trim_whitespace" },
-          filetypes = { "_" },
-        },
-        {
-          formatter = { "stylua" },
-          filetypes = { "lua" },
-        },
-        {
-          formatter = { { "prettierd", "prettier" } },
-          filetypes = {
-            "html",
-            "javascript",
-            "javascript.jsx",
-            "javascriptreact",
-            "json",
-            "typescript",
-            "typescript.tsx",
-            "typescriptreact",
-            "yaml",
-          },
+      {
+        formatter = { "trim_whitespace" },
+        filetypes = { "_" },
+      },
+      {
+        formatter = { "stylua" },
+        filetypes = { "lua" },
+      },
+      {
+        formatter = { { "prettierd", "prettier" } },
+        filetypes = {
+          "html",
+          "javascript",
+          "javascript.jsx",
+          "javascriptreact",
+          "json",
+          "typescript",
+          "typescript.tsx",
+          "typescriptreact",
+          "yaml",
         },
       },
     },
     config = function(_, opts)
       local formatters_by_ft = {}
-      for _, config in ipairs(opts.configs) do
+      for _, config in ipairs(opts) do
         for _, filetype in ipairs(config.filetypes) do
           formatters_by_ft[filetype] = config.formatter
         end

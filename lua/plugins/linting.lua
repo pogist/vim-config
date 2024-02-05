617caedf480e0c97b826ca845bb1c9f45d3dd27b
pogist <murilo.paixao.2@gmail.com>
2024-02-05 17:58:00 -0300

    enable conditional linter use based on the presence of certain files

--- lua/plugins/linting.lua
+++ lua/plugins/linting.lua
@@ -3,7 +3,12 @@ return {
     "mfussenegger/nvim-lint",
     event = { "BufNewFile", "BufReadPost" },
     opts = {
-      events = { "BufWritePost", "BufReadPost", "InsertLeave", "TextChanged" },
+      events = {
+        "BufReadPost",
+        "BufWritePost",
+        "InsertLeave",
+        "TextChanged",
+      },
       {
         linter = { "eslint_d" },
         filetypes = {
@@ -14,19 +19,32 @@ return {
           "typescript.tsx",
           "typescriptreact",
         },
+        condition = {
+          ".eslintrc.js",
+          ".eslintrc.cjs",
+          ".eslintrc.yaml",
+          ".eslintrc.yml",
+          ".eslintrc.json",
+        },
       },
     },
     config = function(_, opts)
       local util = require("util")
       local lint = require("lint")
 
-      local linters_by_ft = {}
-      for _, config in ipairs(opts) do
-        for _, filetype in ipairs(config.filetypes) do
-          linters_by_ft[filetype] = config.linter
-        end
+      local bufname = vim.api.nvim_buf_get_name(0)
+      local function is_satisfied(condition)
+        return vim.fs.find(condition, { path = bufname, upward = true })[1] ~= nil
       end
 
+      local linters_by_ft = {}
+      for _, config in ipairs(opts) do
+        if not config.condition or is_satisfied(config.condition) then
+          for _, filetype in ipairs(config.filetypes) do
+            linters_by_ft[filetype] = config.linter
+          end
+        end
+      end
       lint.linters_by_ft = linters_by_ft
 
       vim.api.nvim_create_autocmd(opts.events, {

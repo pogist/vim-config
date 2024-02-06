ff79b2cee960ba533a0ff46bc6f5787a594cabc4
pogist <murilo.paixao.2@gmail.com>
2024-02-06 11:06:08 -0300

    look for conditional files from bufpath up to homedir when linting

--- lua/plugins/linting.lua
+++ lua/plugins/linting.lua
@@ -32,21 +32,27 @@ return {
       local util = require("util")
       local lint = require("lint")
 
-      local bufname = vim.api.nvim_buf_get_name(0)
-      local function is_satisfied(condition)
-        return vim.fs.find(condition, { path = bufname, upward = true })[1] ~= nil
+      local homedir = vim.uv.os_homedir()
+      local bufpath = vim.fs.dirname(vim.api.nvim_buf_get_name(0))
+      local function satisfies(condition)
+        local found = vim.fs.find(condition, {
+          upward = true,
+          path = bufpath,
+          stop = homedir,
+        })
+        return found[1] ~= nil
       end
 
       local linters_by_ft = {}
       for _, config in ipairs(opts) do
-        if not config.condition or is_satisfied(config.condition) then
+        if not config.condition or satisfies(config.condition) then
           for _, filetype in ipairs(config.filetypes) do
             linters_by_ft[filetype] = config.linter
           end
         end
       end
-      lint.linters_by_ft = linters_by_ft
 
+      lint.linters_by_ft = linters_by_ft
       vim.api.nvim_create_autocmd(opts.events, {
         group = vim.api.nvim_create_augroup("NvimLint", { clear = true }),
         callback = util.debounce(100, function()
